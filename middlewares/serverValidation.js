f916a1a2c4dda6d767bfb5ca32c21af1a4ebdb3c
Anna Konovalova <konon.ana@yandex.ru>
2022-04-10 21:10:21 +0100

    assertation fixed

--- middlewares/serverValidation.js
+++ middlewares/serverValidation.js
@@ -41,8 +41,8 @@ const createCardValid = celebrate({
 
 const userValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 });
 
